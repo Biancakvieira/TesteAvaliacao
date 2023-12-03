9c017ed36bd1c4e1fdb1871652d7020ecd1e0610
Biancakvieira <112039833+Biancakvieira@users.noreply.github.com>
2023-12-03 19:31:35 -0300

    Add files via upload

---
+++ src/test/java/pages/CadastroPagesPF_DN_ERRO.java
@@ -0,0 +1,54 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.time.Duration;
+
+public class CadastroPagesPF_DN_ERRO {
+    static WebDriver driver;
+
+    public CadastroPagesPF_DN_ERRO(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void preencherCampoPF_DN_ERRO() {
+
+        WebElement Nome = driver.findElement(By.id("name"));
+        Nome.sendKeys("Vitoria");
+
+        WebElement sobrenome = driver.findElement(By.id("lastname"));
+        sobrenome.sendKeys("Invencivel");
+
+        WebElement razao_Social = driver.findElement(By.id("companyName"));
+        razao_Social.sendKeys("VitoriaInvencivel");
+
+        WebElement Data_de_abertura_da_empresa
+                = driver.findElement(By.id("date"));
+        Data_de_abertura_da_empresa.sendKeys("11112020");
+
+        WebElement cnpj = driver.findElement(By.id("document"));
+        cnpj.sendKeys("70.380.218/0001-02");
+
+        WebElement ddi = driver.findElement(By.xpath("//body/div[@id='root-app']/div[1]/div[1]/form[1]/div[1]/div[3]/div[6]/div[1]/div[1]/a[1]/span[2]"));
+        ddi.click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+
+        WebElement ddiSelecionado = driver.findElement(By.xpath(" //a[contains(text(),'Brasil - +55')]"));
+        ddiSelecionado.click();
+
+        WebElement ddd = driver.findElement(By.id("ddd"));
+        ddd.sendKeys("51");
+
+        WebElement telefone = driver.findElement(By.id("phone"));
+        telefone.sendKeys("998966255");
+
+        WebElement email = driver.findElement(By.id("email"));
+        email.sendKeys("vitoriainvencivel1@gmail.com");
+
+        WebElement confEmail = driver.findElement(By.id("confEmail"));
+        confEmail.sendKeys("vitoriainvencivel1@gmail.com");
+
+    }
+}
